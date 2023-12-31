a36cb8c24e4acf3b9cffeb1f533490f4cfc77a4c
JSG <jsgcys0815@gmail.com>
2024-01-01 04:11:44 +0900

    jwt login

---
+++ src/main/java/com/fridgeBE/fridge/dto/UserLoginResponseDto.java
@@ -0,0 +1,17 @@
+package com.fridgeBE.fridge.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserLoginResponseDto {
+    private String accessToken;
+    private String refreshToken;
+
+    private int userId;
+}
